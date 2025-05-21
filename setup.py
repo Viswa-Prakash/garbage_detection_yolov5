dcc0953a7d6b564f4cd5c5161da912f29cfab358
“Viswa <prakashviswa1990@gmail.com>
2025-05-21 11:21:26 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'garbage',
+    version= '0.0.0',
+    author= 'Viswa-Prakash',
+    author_email= 'prakashviswa1990@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
